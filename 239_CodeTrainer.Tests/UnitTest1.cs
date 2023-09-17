c10baffa68b316f67a3da3e1c5eae4b2260067ee
Maksim <jdwizzard@gmail.com>
2023-09-17 04:23:58 +0200

    Add project files.

---
+++ 239_CodeTrainer.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace _239_CodeTrainer.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ 239_CodeTrainer/MainClass.cs
@@ -0,0 +1,30 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System;
+
+// Do not change the class name
+class MainClass
+{
+
+    // COMPLETE THIS FUNCTION
+    static void process(string[] arr, int size)
+    {
+        Console.WriteLine(string.Join("\n", arr));
+    }
+
+    // you do not need to edit anything below this line.
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] arr = input.Split(' ');
+
+        process(arr, arr.Length);
+    }
+}
